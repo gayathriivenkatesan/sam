feaf792619dc368c1f6c9f788a55e3a4570f8dea
gayathriivenkatesan <47655018+gayathriivenkatesan@users.noreply.github.com>
2020-01-27 12:04:10 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,24 @@
-print('Gayu')
+class Base(object): 
+  
+    # Constructor 
+    def __init__(self, x): 
+        self.x = x
+        print(self.x)
+  
+class Derived(Base): 
+    # Constructor 
+    def __init__(self, x, y): 
+        Base.__init__(self,10) 
+        self.y = self.x
+        print(self.y)
+  
+    def printXY(self): 
+       
+       # print(self.x, self.y) will also work 
+       print(self.y) 
+  
+  
+# Driver Code 
+
+b=Derived(20,10)
+b.printXY()  
